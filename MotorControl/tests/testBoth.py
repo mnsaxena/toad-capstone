c136c733aff7a599f9a14171d64f673b1ac4518d
mnsaxena <54684255+mnsaxena@users.noreply.github.com>
2022-01-20 10:50:44 -0500

    Add files via upload

---
+++ MotorControl/tests/testBoth.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import time
+from TOAD.MotorControl.RearMotors import motors, MAX_SPEED
+
+# Define a custom exception to raise if a fault is detected.
+class DriverFault(Exception):
+    def __init__(self, driver_num):
+        self.driver_num = driver_num
+
+def raiseIfFault():
+    if motors.motor1.getFault():
+        raise DriverFault(1)
+    if motors.motor2.getFault():
+        raise DriverFault(2)
+
+# Set up sequences of motor speeds.
+test_forward_speeds = list(range(0, MAX_SPEED, 1)) + \
+  [MAX_SPEED] * 200 + list(range(MAX_SPEED, 0, -1)) + [0]  
+
+test_reverse_speeds = list(range(0, -MAX_SPEED, -1)) + \
+  [-MAX_SPEED] * 200 + list(range(-MAX_SPEED, 0, 1)) + [0]  
+
+try:
+    motors.setSpeeds(0, 0)
+
+    print("Motor 1 forward")
+    for s in test_forward_speeds:
+        motors.setSpeeds(s,s)
+        raiseIfFault()
+        time.sleep(0.002)
+
+    print("Motor 1 reverse")
+    for s in test_reverse_speeds:
+        motors.setSpeeds(s,s)
+        raiseIfFault()
+        time.sleep(0.002)
+
+    # Disable the drivers for half a second.
+    motors.disable()
+    time.sleep(0.5)
+    motors.enable()
+
+except DriverFault as e:
+    print("Driver %s fault!" % e.driver_num)
+
+finally:
+  # Stop the motors, even if there is an exception
+  # or the user presses Ctrl+C to kill the process.
+    motors.forceStop()
